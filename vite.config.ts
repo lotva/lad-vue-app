b4666e4e2b999c029cae1417b0ac6fc093fa03a7
lotva <dlotva@gmail.com>
2024-08-07 21:15:26 +0300

    Редактирует browserslist

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     css: {
         transformer: 'lightningcss',
         lightningcss: {
-            targets: browserslistToTargets(browserslist('>= 0.25%')),
+            targets: browserslistToTargets(browserslist('> 0.2% and not dead')),
         },
     },
 })
