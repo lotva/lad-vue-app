a8b4ae3fb9b110d276633632a3460b0a468cc271
lotva <dlotva@gmail.com>
2024-08-10 00:47:13 +0300

    Добавляет инлайн-погоду для вставки в хедер

---
+++ src/components/WeatherWidget/WeatherInline.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import WeatherTemperature from './WeatherTemperature.vue'
+
+    import { useWeatherStore } from '@/stores/weatherStore'
+
+    const weatherStore = useWeatherStore()
+</script>
+
+<template>
+    <template v-if="weatherStore.cityName">
+        <aside aria-label="Погода сейчас">
+            <p>{{ weatherStore.cityName }} — <WeatherTemperature></WeatherTemperature></p>
+        </aside>
+    </template>
+</template>
---
+++ src/components/WeatherWidget/WeatherTemperature.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+    import { useFormattedWeather } from './useFormattedWeather'
+
+    const { computedTemperature } = useFormattedWeather()
+</script>
+
+<template>{{ computedTemperature }}&thinsp;°C</template>
