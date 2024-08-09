1f09879c2a3bca21a84ad27802c4cd584cc9ab6b
lotva <dlotva@gmail.com>
2024-08-10 01:05:12 +0300

    Задаёт структуру роутинга

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import RootView from '@/views/RootView.vue'
 import WeatherView from '@/views/WeatherView.vue'
+import ArticlesView from '@/views/ArticlesView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: '/',
+            component: RootView,
+            name: 'RootRoute',
+            meta: {
+                title: 'Вью-блог',
+                description: 'Приложение на Вью с разделами «Погода» и «Статьи»',
+                navigation: true,
+            },
+        },
         {
             path: '/weather',
             component: WeatherView,
@@ -13,6 +25,19 @@ const router = createRouter({
                 title: 'Погода',
                 description:
                     'Приложение взаимодействует с OpenWeatherMap API и выводит погоду по названию города.',
+                navigation: true,
+                isSection: true,
+            },
+        },
+        {
+            path: '/articles',
+            component: ArticlesView,
+            name: 'ArticlesRoute',
+            meta: {
+                title: 'Статьи',
+                description: 'Приложение выводит список статей.',
+                navigation: true,
+                isSection: true,
             },
         },
     ],
